e027ceb15abae603cb7043d0751f0e684ecce0e9
John R <john.l.ritucci@gmail.com>
2017-12-02 20:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
+}
 
+function deleteFromObjectByKey(object, key){}
+
+  delete object.key;
+  return object
 }
